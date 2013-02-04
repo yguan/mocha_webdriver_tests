0d309b1c182cdc7c1062c814572077873144cf78
Yong Guan <gyong9@gmail.com>
2013-02-03 21:34:51 -0800

    add injectJs for injecting js file to a page

---
+++ src/injectJs.js
@@ -0,0 +1,6 @@
+function injectJs(link) {
+    var scr = document.createElement('script');
+    scr.type = 'text/javascript';
+    scr.src = link;
+    document.getElementsByTagName('head')[0].appendChild(scr)
+}
